3a5befeff7e407e570945d23045f5483197680e4
Harry Garrood <harry@garrood.me>
2015-11-13 17:41:55 +0100

    Use unsafePerformEff inside unsafeThrowException

--- src/Control/Monad/Eff/Exception/Unsafe.purs
+++ src/Control/Monad/Eff/Exception/Unsafe.purs
@@ -8,10 +8,7 @@ import Control.Monad.Eff.Exception
 -- | Throw an exception in pure code. This function should be used very
 -- | sparingly, as it can cause unexpected crashes at runtime.
 unsafeThrowException :: forall a. Error -> a
-unsafeThrowException = unsafeRunEff <<< throwException
-  where
-  unsafeRunEff :: forall e b. Eff e b -> b
-  unsafeRunEff = runPure <<< unsafeInterleaveEff
+unsafeThrowException = unsafePerformEff <<< throwException
 
 -- | Defined as `unsafeThrowException <<< error`.
 unsafeThrow :: forall a. String -> a
